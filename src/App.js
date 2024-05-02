3aedb8bb2ea184e40e592142d74b4691dee144c5
mayteles <maylon.ferreira@icloud.com>
2024-05-01 22:11:00 -0700

    nav structure and Link path applied

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './componets/header/Header';
+import OrderHistory from './componets/OrderHistory/OrderHistory';
+import Cart from './componets/Cart/Cart';
 
 function App() {
   return (
-    <>
-      <Router>
+    <Router>
+      <div>
         <Header />
-      </Router>
-    </>
+        <Routes>
+          <Route path='/OrderHistory' element={<OrderHistory />} />
+          <Route path='/Cart' element={<Cart />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/componets/header/Header.js
+++ src/componets/header/Header.js
@@ -1,53 +1,37 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom"
+import logo from '../../assets/images/logo.png'
+import search from '../../assets/icons/search.svg'
+import pin from '../../assets/icons/map-pin.png'
+import cart from '../../assets/icons/fast-cart.png'
 
 function Header() {
   return (
-    <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </>
+    <nav>
+      <div>
+        <Link to={"/"} ><img src={logo} alt='website logo'/></Link>
+        <div>
+          <img src={pin} alt='Map Pin'/>
+          <Link to={"/some-link"}>Delivery to Vancouver</Link>
+        </div>
+      </div>
+      <div>
+        <input type='text' id="searchInput" placeholder='Search Bobbuilds.ca'/>
+        <img src={search} alt='Search Icon' />
+      </div>
+      <div>
+        <Link to={"/OrderHistory"}><button>Returns & Orders</button></Link>
+        <Link to={"/Cart"}>
+          <div>
+            <span>0</span>
+            <img src={cart} alt='shopping cart'/>
+          </div>
+          <div>
+            <p>Cart</p>
+          </div>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
