fd5cd9b00359eb70e7462504cade1f1c83d3f8b3
mayteles <maylon.ferreira@icloud.com>
2024-05-02 19:04:55 -0700

    navbar design implement and div deleted from Header.js

--- src/componets/header/Header.js
+++ src/componets/header/Header.js
@@ -12,28 +12,20 @@ import cart from '../../assets/icons/fast-cart.png';
 function Header() {
   return (
     <nav className='nav'>
-      <div>
+      <div className='nav__b1'>
         <Link to={"/"} ><img src={logo} alt='website logo'/></Link>
-        <div>
+        <div className='nav__b1-sub'>
           <img src={pin} alt='Map Pin'/>
-          <Link to={"/some-link"}>Delivery to Vancouver</Link>
+          <Link to={"/some-link"} className='link-no-decoration'>Delivery to Vancouver</Link>
         </div>
       </div>
       <div>
         <input type='text' id="searchInput" placeholder='Search Bobbuilds.ca'/>
         <img src={search} alt='Search Icon' />
       </div>
-      <div>
+      <div className='nav__b3'>
         <Link to={"/OrderHistory"}><button>Returns & Orders</button></Link>
-        <Link to={"/Cart"}>
-          <div>
-            <span>0</span>
-            <img src={cart} alt='shopping cart'/>
-          </div>
-          <div>
-            <p>Cart</p>
-          </div>
-        </Link>
+        <Link className='link-no-decoration' to={"/Cart"}><p><span>0</span> Cart</p></Link>
       </div>
     </nav>
   )
