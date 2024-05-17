d50a20f37012f51c03b7c53ca901f000c030153a
mayteles <maylon.ferreira@icloud.com>
2024-05-17 20:34:24 +0530

    added basic step with register and login routes

---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../../utils/api";
+import axios from "axios";
+
+const LoginPage = () => {
+  const nav = useNavigate();
+  let defaultData = {
+    email: "",
+    password: "",
+  };
+  const [userData, setUserData] = useState(defaultData);
+
+  const handleLoginUser = async (e) => {
+    e.preventDefault();
+    try {
+      let data = await axios.post(`${API_URL}/auth/login`, userData);
+      localStorage.setItem("user-ecommerce-auth-token", data.data.token);
+      localStorage.setItem("user-ecommerce", JSON.stringify(data.data.user));
+      alert(data.data.message);
+      setUserData(defaultData);
+      nav("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLoginUser} className="register_form">
+        <input
+          placeholder="Enter Email"
+          value={userData.email}
+          type="text"
+          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        />
+        <input
+          placeholder="Enter Password"
+          value={userData.password}
+          type="password"
+          onChange={(e) =>
+            setUserData({ ...userData, password: e.target.value })
+          }
+        />
+        <button onClick={handleLoginUser}>Login</button>
+        <p className="clickable-paragraph" onClick={() => nav("/register")}>
+          New user ? Signup Now !
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./RegisterPage.scss";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../../utils/api";
+const RegisterPage = () => {
+  let defaultData = {
+    name: "",
+    email: "",
+    password: "",
+  };
+  const [userData, setUserData] = useState(defaultData);
+  const nav = useNavigate();
+
+  const handleUserRegister = async (e) => {
+    e.preventDefault()
+    try {
+      let data = await axios.post(`${API_URL}/auth/register`, userData);
+      alert(data.data.message)
+      setUserData(defaultData)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleUserRegister} className="register_form">
+        <input
+          required
+          placeholder="Enter Name"
+          value={userData.name}
+          type="text"
+          onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+        />
+        <input
+          required
+          placeholder="Enter Email"
+          value={userData.email}
+          type="text"
+          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        />
+        <input
+          required
+          placeholder="Enter Password"
+          value={userData.password}
+          type="password"
+          onChange={(e) => setUserData({ ...userData, password: e.target.value })}
+        />
+        <button onClick={handleUserRegister}>Register</button>
+        <p className="clickable-paragraph" onClick={() => nav("/login")}>
+          Alread have account ? Login Now !
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default RegisterPage;
